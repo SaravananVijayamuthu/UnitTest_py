b52df8059608a658941f96097fd543418789ac54
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-20 12:10:15 +0530

    Checked unittest on 3 module types

--- test.py
+++ test.py
@@ -4,14 +4,26 @@ try:
 except Exception as e:
     print(format(e))
 
-#Test through api status
+# Test through api status
 class TestAPI(unittest.TestCase):
     def test1(self):
         tester = app.test_client(self)
-        response  = tester.get("/hi")
+        response = tester.get("/hi")
         statuscode = response.status_code
-        self.assertEqual(statuscode, 404)
-        
+        self.assertEqual(statuscode, 200)
+
+    # check content
+    def test1_content(self):
+        tester = app.test_client(self)
+        response = tester.get("/hi")
+        self.assertEqual(response.content_type, "application/json")
+
+    # Data
+    def test1_Data(self):
+        tester = app.test_client(self)
+        response = tester.get("/hi")
+        self.assertTrue(b"Saravanan Vijayamuthu" in response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
